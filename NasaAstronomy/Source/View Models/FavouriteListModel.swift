59a92875e98ca8b3e07541952b43b47ced286376
Sugeet Goyal <sugeetgoyalbilha@gmail.com>
2022-04-13 03:40:13 +0530

    implemented favourite list manager and video loading

---
+++ NasaAstronomy/Source/View Models/FavouriteListModel.swift
@@ -0,0 +1,8 @@
+//
+//  FavouriteListModel.swift
+//  NasaAstronomy
+//
+//  Created by Sugeet Goyal on 13/04/22.
+//
+
+import Foundation
---
+++ NasaAstronomy/Source/Views/FavouriteTableViewController.swift
@@ -0,0 +1,8 @@
+//
+//  FavouriteTableViewController.swift
+//  NasaAstronomy
+//
+//  Created by Sugeet Goyal on 13/04/22.
+//
+
+import Foundation
